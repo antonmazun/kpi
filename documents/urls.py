13e0e2ec25cddeee3c8b119974e5dd14a07eca68
antonmazun <kpi.study1@gmail.com>
2018-05-29 00:54:19 +0300

    'doc1'

--- documents/urls.py
+++ documents/urls.py
@@ -2,5 +2,6 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     path('', views.all_documents),
+    path('statement_state_registration/<int:id_one>', views.add_statement_state_registration , name = 'statement_state_registration'),
 
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
+from user.models import PhysicalUser
 # Create your views here.
 
 
-def all_documents(request):
-    # id_user  = request.session['entry_user']
-    print("!!!!!!!!!!!!!!" , request.path)
+def all_documents(request , id_one):
+    print(id_one)
+    u = PhysicalUser.objects.get(id=id_one)
+    ctx  = {}
+    ctx['user'] = u
+    return render(request , 'all_document.html', ctx)
 
-    return render(request , 'all_document.html', {})
+def add_statement_state_registration(request , id):
+    u = PhysicalUser.objects.get(id=id)
+    ctx = {}
+    ctx['user'] = u
+    return  render(request , 'statement_state_registration_form.html' ,ctx)
